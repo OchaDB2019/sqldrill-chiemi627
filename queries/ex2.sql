c6402e0d62e6a0263254eb88654c51f103f7df05
Chiemi Watanabe <chiemi627@users.noreply.github.com>
2019-11-12 14:27:14 +0900

    ケアレスミスを修正

--- queries/ex2.sql
+++ queries/ex2.sql
@@ -2,6 +2,7 @@
 # 'SHARP シャープ株式会社' さんをフォローする人のアカウント名を求めよ。
 # この下の行にSQL文を書きましょう。
 
-SELECT follower_account
-FROM follow
-WHERE followee_account = 'SHARP シャープ株式会社'
+SELECT f.follower_account
+FROM follow f, user u
+WHERE f.followee_account = u.account
+  and u.name = 'SHARP シャープ株式会社'
